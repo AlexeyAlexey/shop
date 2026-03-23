227910862f0648b0f80649284a65a8637a4f4e4c
Alexey <Alexey>
2026-03-23 15:30:33 +0200

    view list of products

--- db/migrate/20260323123827_create_products.rb
+++ db/migrate/20260323123827_create_products.rb
@@ -1,6 +1,6 @@
 class CreateProducts < ActiveRecord::Migration[8.1]
   def change
-    create_table :products do |t|
+    create_table :products, id: :uuid do |t|
       t.string :title
       t.string :description
       t.string :preview_img
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_23_100605) do
-  create_table "products", force: :cascade do |t|
+ActiveRecord::Schema[8.1].define(version: 2026_03_23_123827) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+  enable_extension "pgcrypto"
+
+  create_table "products", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "description"
     t.string "preview_img"
